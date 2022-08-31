de37b8dd8b89d339fd8da3f83bc16d01c4e64700
tony <tony@Mac.local>
2022-08-31 12:06:12 +0200

    project create

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use App\Models\Skill;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 
 class ProjectController extends Controller
@@ -37,7 +39,24 @@ class ProjectController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'image' => ['required', 'image'],
+            'name' => ['required', 'min:3'],
+            'skill_id' => ['required']
+        ]);
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image')->store('projects');
+            Project::create([
+                'skill_id' => $request->skill_id,
+                'name' => $request->name,
+                'image' => $image,
+                'project_url' => $request->project_url
+            ]);
+
+            return Redirect::route('projects.index');
+        }
+        return Redirect::back();
     }
 
     /**
--- resources/js/Pages/Projects/Create.vue
+++ resources/js/Pages/Projects/Create.vue
@@ -1,5 +1,5 @@
 <template>
-  <Head title="New Project" />
+  <Head title="New Project  " />
   <BreezeAuthenticatedLayout>
     <template #header>
       <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -8,12 +8,101 @@
     </template>
 
     <div class="py-12">
-      <div class="max-w-md mx-auto sm:px-6 lg:px-8">Form</div>
+      <div class="max-w-md mx-auto sm:px-6 lg:px-8 bg-white">
+        <form class="p-4" @submit.prevent="submit">
+          <div>
+            <BreezeLabel for="skill_id" value="Skill" />
+            <select
+              v-model="form.skill_id"
+              id="skill_id"
+              name="skill_id"
+              class="
+                mt-1
+                block
+                w-full
+                pl-3
+                pr-10
+                py-2
+                text-base
+                border-gray-300
+                focus:outline-none focus:ring-indigo-500 focus:border-indigo-500
+                sm:text-sm
+                rounded-md
+              "
+            >
+              <option v-for="skill in skills" :key="skill.id" :value="skill.id">
+                {{ skill.name }}
+              </option>
+            </select>
+          </div>
+          <div>
+            <BreezeLabel for="name" value="Name" />
+            <BreezeInput
+              id="name"
+              type="text"
+              class="mt-1 block w-full"
+              v-model="form.name"
+              autofocus
+              autocomplete="name"
+            />
+            <BreezeInputError class="mt-2" :message="form.errors.name" />
+          </div>
+          <div>
+            <BreezeLabel for="project_url" value="URL" />
+            <BreezeInput
+              id="project_url"
+              type="text"
+              class="mt-1 block w-full"
+              v-model="form.project_url"
+              autocomplete="projecturl"
+            />
+            <BreezeInputError class="mt-2" :message="form.errors.project_url" />
+          </div>
+          <div class="mt-2">
+            <BreezeLabel for="image" value="Image" />
+            <BreezeInput
+              id="image"
+              type="file"
+              class="mt-1 block w-full"
+              @input="form.image = $event.target.files[0]"
+            />
+            <BreezeInputError class="mt-2" :message="form.errors.image" />
+          </div>
+          <div class="flex items-center justify-end mt-4">
+            <BreezeButton
+              class="ml-4"
+              :class="{ 'opacity-25': form.processing }"
+              :disabled="form.processing"
+            >
+              Store
+            </BreezeButton>
+          </div>
+        </form>
+      </div>
     </div>
   </BreezeAuthenticatedLayout>
 </template>
 
 <script setup>
 import BreezeAuthenticatedLayout from "@/Layouts/Authenticated.vue";
-import { Head } from "@inertiajs/inertia-vue3";
+import { Head, useForm } from "@inertiajs/inertia-vue3";
+import BreezeButton from "@/Components/Button.vue";
+import BreezeInput from "@/Components/Input.vue";
+import BreezeInputError from "@/Components/InputError.vue";
+import BreezeLabel from "@/Components/Label.vue";
+
+defineProps({
+  skills: Array,
+});
+
+const form = useForm({
+  name: "",
+  image: null,
+  skill_id: "",
+  project_url: "",
+});
+
+const submit = () => {
+  form.post(route("projects.store"));
+};
 </script>
